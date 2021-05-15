4ed017d256cf6fed6a8fb9749169a51f0ad421a4
Iulia Gheorghe <iulia.gheorghe25@192-168-0-102.rdsnet.ro>
2021-05-16 01:48:41 +0300

    Video Broken Stuff

--- JS Files/menu.js
+++ JS Files/menu.js
@@ -1,23 +1,26 @@
-    document.getElementsByClassName("menuElement")[pageNo].style.color="rgb(255, 62, 108)";
+    if(pageNo!==null)
+    {
+      document.getElementsByClassName("menuElement")[pageNo].style.color="rgb(255, 62, 108)";
 
     document.getElementById("mobile-menu").getElementsByTagName("a")[pageNo].style.backgroundColor="rgb(255, 62, 108)";
     document.getElementById("mobile-menu").getElementsByTagName("a")[pageNo].style.color="#fff";
+  }
 
 
     function menuHover(x){
       document.getElementsByClassName("menuElement")[x].style.color="rgb(255, 80, 122)";
       document.getElementsByClassName("menuElementExp")[x].style.display="flex";
 
-      if(pageNo!==0)
       document.getElementById("contentOverlay").style.display="flex";
     }
 
     function menuHoverOut(x){
       document.getElementsByClassName("menuElement")[x].style.color="#fff";
       document.getElementsByClassName("menuElementExp")[x].style.display="none";
+      if(pageNo!==null)
       document.getElementsByClassName("menuElement")[pageNo].style.color="rgb(255, 62, 108)";
       
-      if(pageNo!==0)
+
       document.getElementById("contentOverlay").style.display="none";
     }
 
