e42f69922225b07d91eca86462983d2269406685
Iulia Gheorghe <iulia.gheorghe25@192-168-0-102.rdsnet.ro>
2021-05-16 18:26:05 +0300

    SlideShow with Circles

--- JS Files/slideshow.js
+++ JS Files/slideshow.js
@@ -12,18 +12,62 @@ let imagesSources=[
 
 let curentIndex=0;
 let curentImage=document.getElementById("slideshow").getElementsByTagName("img")[0];
+let circlesArray=document.getElementById("circles").getElementsByTagName("i");
+let lastIndex=0;
 
+
+function checkCurentIndex(){
+
+	for(let i=0;i<circlesArray.length;i++)
+
+		// console.log(i);
+		// console.log(circlesArray[i].className);
+		// console.log(circlesArray[i].className.includes("active"));
+			if(circlesArray[i].className.includes("active"))
+				lastIndex=i;
+
+			// console.log(lastIndex);
+
+			return lastIndex;
+}
+
+function circlesMagic(x){
+
+	// console.log(circlesArray);
+
+	checkCurentIndex();
+
+	circlesArray[x].className = circlesArray[x].className.replace("fa-circle-o", "fa-circle");
+	circlesArray[x].className = circlesArray[x].className.replace("inactiv", "active");
+	circlesArray[lastIndex].className = circlesArray[lastIndex].className.replace("fa-circle", "fa-circle-o");
+	circlesArray[lastIndex].className = circlesArray[lastIndex].className.replace("active", "inactiv");
+}
+
+function showImage(x){
+	curentImage.src=imagesSources[x];
+	circlesMagic(x);
+}
 
 function previousImage(){
+
+	curentIndex=checkCurentIndex();
+
+	lastIndex=curentIndex;
+	
 	if (curentIndex==0) curentIndex=6;
 	else
 	curentIndex--;
-	curentImage.src=imagesSources[curentIndex];
+	showImage(curentIndex);
 }
 
 function nextImage(){
+
+	curentIndex=checkCurentIndex();
+
+	lastIndex=curentIndex;
+	
 	if (curentIndex==6) curentIndex=0;
 	else
 	curentIndex++;
-	curentImage.src=imagesSources[curentIndex];
+	showImage(curentIndex);
 }
